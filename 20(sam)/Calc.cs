6624e1b044c18e03ad176b695c53609d1a113eaa
Markow Kirill <122300549+pank4roker@users.noreply.github.com>
2024-04-25 21:10:27 +0300

    2 задание(калькулятор)

---
+++ 20(sam)/Calc.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_1_
+{
+    class Calc<T, K>
+    {
+        public T Value1 { get; set; }
+        public K Value2 { get; set; }
+        public Calc() { }
+        public Calc(T value1, K value2) 
+        { 
+          Value1 = value1; 
+          Value2 = value2; 
+        }
+        public void Add()
+        {
+            if (typeof(T) == typeof(int) || typeof(K) == typeof(int) ||  typeof(T) == typeof(double) || typeof(K) == typeof(double)  || typeof(T) == typeof(string) || typeof(K) == typeof(string))
+            {
+                double a = Convert.ToDouble(Value1);
+                double b = Convert.ToDouble(Value2);
+                Console.WriteLine($"{Value1} + {Value2} = {a + b}");
+            }
+            else
+            {
+                Console.WriteLine("Неверный тип данных");
+            }
+        }
+        public void Sub()
+        {
+            if (typeof(T) == typeof(int) || typeof(K) == typeof(int) || typeof(T) == typeof(double) || typeof(K) == typeof(double) || typeof(T) == typeof(string) || typeof(K) == typeof(string))
+            {
+                double a = Convert.ToDouble(Value1);
+                double b = Convert.ToDouble(Value2);
+                Console.WriteLine($"{Value1} - {Value2} = {a - b}");
+            }
+            else
+            {
+                Console.WriteLine("Неверный тип данных");
+            }
+        }
+        public void Div()
+        {
+            if (typeof(T) == typeof(int) || typeof(K) == typeof(int) || typeof(T) == typeof(double) || typeof(K) == typeof(double) || typeof(T) == typeof(string) || typeof(K) == typeof(string))
+            {
+                double a = Convert.ToDouble(Value1);
+                double b = Convert.ToDouble(Value2);
+                Console.WriteLine($"{Value1} / {Value2} = {a / b:f2}");
+            }
+            else
+            {
+                Console.WriteLine("Неверный тип данных");
+            }
+        }
+        public void Mul()
+        {
+            if (typeof(T) == typeof(int) || typeof(K) == typeof(int) || typeof(T) == typeof(double) || typeof(K) == typeof(double) || typeof(T) == typeof(string) || typeof(K) == typeof(string))
+            {
+                double a = Convert.ToDouble(Value1);
+                double b = Convert.ToDouble(Value2);
+                Console.WriteLine($"{Value1} * {Value2} = {a * b:f2}");
+            }
+            else
+            { 
+                Console.WriteLine("Неверный тип данных");
+            }
+        }
+
+    }
+}
---
+++ 20(sam)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calc<int, int> calculator = new Calc<int, int>(5, 2);
+            Console.WriteLine("1 Объект(int/int)");
+            calculator.Add();
+            calculator.Sub();
+            calculator.Mul();
+            calculator.Div();
+            Calc<int, double> calculator1 = new Calc<int, double>(2, 4.5);
+            Console.WriteLine("2 Объект(int/double)");
+            calculator1.Add();
+            calculator1.Sub();
+            calculator1.Mul();
+            calculator1.Div();
+            Calc<int, string> calculator2 = new Calc<int, string>(3, "5");
+            Console.WriteLine("3 Объект(int/string)");
+            calculator2.Add();
+            calculator2.Sub();
+            calculator2.Mul();
+            calculator2.Div();
+            Console.ReadLine();
+        }
+    }
+}
